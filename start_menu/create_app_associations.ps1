8fd47781868573e6ceac5feb1547a973e3653049
Mateusz Dobrzanski <mateuszdobrzasnki@outlook.com>
2019-01-14 20:39:10 +0100

    windows 10 create menu layout/apps association

---
+++ start_menu/create_app_associations.ps1
@@ -0,0 +1,4 @@
+﻿# create, import and delete apps association
+dism /online /Export-DefaultAppAssociations:"%UserProfile%\Desktop\AppAssociations.xml"
+dism /online /Import-DefaultAppAssociations:"%UserProfile%\Desktop\AppAssociations.xml"
+Remove-Item -Path C:\Users\$env:UserName\Desktop\AppAssociations.xml
---
+++ start_menu/start_layout.ps1
@@ -0,0 +1,9 @@
+﻿# export start menu layout from current user
+Export-StartLayout –path C:\Users\$env:UserName\Desktop\LayoutModification.xml
+
+# copy start menu layout to .default user
+# it only works for new windows profile
+Import-StartLayout -LayoutPath C:\Users\$env:UserName\Desktop\LayoutModification.xml -MountPath $env:SystemDrive\
+
+# delete layout file
+Remove-Item -Path C:\Users\$env:UserName\Desktop\LayoutModification.xml
