148a546325f9a0e990e982b2ccc91837a8f051d8
mateuszdobrzanski <mateuszdobrzanski@outlook.com>
2020-11-05 19:30:03 +0100

    Update start_layout.ps1
    prepare start layout  for new user

--- start_menu/start_layout.ps1
+++ start_menu/start_layout.ps1
@@ -4,7 +4,8 @@ Export-StartLayout –path C:\Users\$env:UserName\Desktop\LayoutModification.xml
 # copy start menu layout to .default user
 # it only works for new windows profile
 Import-StartLayout -LayoutPath C:\Users\$env:UserName\Desktop\LayoutModification.xml -MountPath $env:SystemDrive\
-Import-StartLayout -LayoutPath C:\Users\Default\Desktop\LayoutModification.xml -MountPath $env:SystemDrive\
+# copy to current profile, it's necessary for sysprep
+Import-StartLayout -LayoutPath C:\Users\$env:UserName\Desktop\LayoutModification.xml -MountPath C:\Users\$env:UserName\appdata\Local\Microsoft\Windows\Shell\
 
 # delete layout file
 Remove-Item -Path C:\Users\$env:UserName\Desktop\LayoutModification.xml
